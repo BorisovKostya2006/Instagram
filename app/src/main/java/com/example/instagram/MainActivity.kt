90feb2e1d3c540672434eddb875e66af8f600a99
Kostya Borisov <borisov.kostya999@gmail.com>
2025-06-06 16:21:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/instagram/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.instagram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.instagram.ui.theme.InstagramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Instagram()
+        }
+    }
+}
+@Preview
+@Composable
+fun Instagram(){
+    Row (
+        modifier = Modifier.fillMaxWidth()
+
+    ){
+        for (i in 1..4){
+            Box(
+                modifier = Modifier.weight(1f)
+                    .border(width = 1.dp, color = Color.DarkGray)
+            ){
+                Text("$i")
+            }
+        }
+    }
+}
